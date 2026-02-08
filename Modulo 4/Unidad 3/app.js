c559af44bace4567a9e102bf170c2d9df7d9e94f
Giuliana Frugoni <gimafru@gmail.com>
2026-02-08 16:37:55 -0300

    unidad 3

--- Modulo 4/Unidad 3/app.js
+++ Modulo 4/Unidad 3/app.js
@@ -4,6 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var session = require('express-session');
+
+
 require('dotenv').config()
 var pool = require('./models/bd')
 
@@ -22,8 +25,16 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+//MODULO 3 UNIDAD 4 INICIO DE SESION
+app.use(session({
+  secret:'asffhdaasfg782hdah289fes',
+  resave: false,
+  saveUninitialized: true
+}));
+
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
+
 
  
 // catch 404 and forward to error handler
